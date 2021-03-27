655d86a0b155691f4463abe8a0e54ea1d9bac2cd
김종진 <martia0118@gmail.com>
2021-03-27 10:37:24 +0900

    앱의 시작화면을 splashactivity로 변경을 했다

---
+++ app/src/main/java/com/nepplus/myzigbangcopy_20210326/SplashActivity.kt
@@ -0,0 +1,11 @@
+package com.nepplus.myzigbangcopy_20210326
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
